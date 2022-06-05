7969c436e1a5fe41464b8f6359cea24b51a45124
tanboys <103524923+tanboys@users.noreply.github.com>
2022-06-05 22:43:48 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-echo $'#!/bin/bash\nwget -O lop.sh https://raw.githubusercontent.com/tanboys/lopay/main/lop.sh && chmod +x lop.sh && bash ./lop.sh' > cloud-init.txt
+echo $'#!/bin/bash\nwget -O lop.sh https://raw.githubusercontent.com/tanboys/lopay/main/ronald.sh && chmod +x ronald.sh && bash ./ronald.sh' > cloud-init.txt
 az group create --name switzerlandnorth --location switzerlandnorth
 az group create --name eastus --location eastus
 az group create --name eastus2 --location eastus2
